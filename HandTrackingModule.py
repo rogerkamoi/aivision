f9a00432baabdff91e4d7defea5c8af14671f670
Roger  Kamoi <roger.kamoi@gmail.com>
2021-06-30 12:47:36 -0300

    Add project Volume Ctrl

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -40,7 +40,7 @@ class handDetector():
                 #print(id, cx, cy)
                 lmlist.append([id, cx, cy])
                 if draw:
-                    cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+                    cv2.circle(img, (cx, cy), 7, (255, 0, 0), cv2.FILLED)
 
         return lmlist
 
